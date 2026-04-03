e320d7e3bc3a76d537e4ed9dce10345a4c9a888d
bartlomiej.hryniewski@gmail.com <bartlomiej.hryniewski@gmail.com>
2026-04-03 11:18:55 +0200

    created drone physical model

---
+++ control/drone.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+class Drone:
+    def __init__(self, initial_position, initial_heading, v_max, u_max):
+        """
+        docstring for __init__
+        
+        :param self: 
+        :param initial_position: starting position of the drone, should be a list or array of length 2
+        :param initial_heading: starting heading direction of the drone, should be a list or array of length 2
+        :param v_max: maximal speed of the drone
+        :param u_max: maximal control input (acceleration) of the drone
+
+        this function initialises the physical model of the drone
+        """
+
+        # using symbolic convention from the article
+        # xy position vector
+        self.d = np.array(initial_position, dtype=float)
+
+        # normalisig heading vector
+        a_not_normal = np.array(initial_heading, dtype=float)
+        try:
+            self.a = a_not_normal / np.linalg.norm(a_not_normal)
+        except ZeroDivisionError:
+            raise ValueError("initial heading vector cannot be zero")
+        
+        self.v = 0.0
+        self.v_max = v_max
+        self.u_max = u_max
+
+    def update_state(self, dt, u, v):
+        """
+        docstring for update_state
+        
+        :param self: 
+        :param dt: time step for the update
+        :param u: control input (acceleration) to be applied, should be a scalar
+        :param v: velocity to be applied, should be a scalar
+
+        this function updates the state of the drone based on the control input and velocity
+        """
+
+        # clip speed v to [0, v_max]:
+        self.v = np.clip(v, 0, self.v_max)
+
+        # clip control input u to [-u_max, u_max]:
+        u = np.clip(u, -self.u_max, self.u_max)
+
+        # orthogonalise the control input to the heading direction
+        u = u - np.dot(u, self.a) * self.a
+
+        # euler integration to update the state
+        # derivative of heading:
+        a_dot = u
+        
+        # derivative of position:
+        d_dot = self.v * self.a
+
+        # update state variables based on the time step
+        self.a = self.a + a_dot * dt
+        self.d = self.d + d_dot * dt
+
+        # normalise the heading vector after the update
+        try:
+            self.a = self.a / np.linalg.norm(self.a)
+        except ZeroDivisionError:
+            raise ValueError("heading vector cannot be zero after update")
